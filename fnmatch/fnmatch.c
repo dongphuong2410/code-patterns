c29395c2ec5a2f24c1d5f50dde401c1a95619bb4
Phuong Do <dongphuong2410@yahoo.com>
2017-06-01 15:27:15 +0900

    Finish implement for mixed star case

--- fnmatch/fnmatch.c
+++ fnmatch/fnmatch.c
@@ -8,24 +8,25 @@
 #define MAX_PATH_LEN        256
 #define MAX_DIR_LEVEL       13
 
-#define ITEM_LEN(splits, idx) (splits->item_pos[idx + 1] - splits->item_pos[idx])
-#define ITEM(splits, idx) (splits->str + splits->item_pos[idx])
-#define ITEM_TYPE(splits, idx) (splits->item_type[idx])
+#define NODE_LEN(splits, idx) (splits->item_pos[idx + 1] - splits->item_pos[idx])
+#define NODE(splits, idx) (splits->str + splits->item_pos[idx])
+#define NODE_TYPE(splits, idx) (splits->item_type[idx])
 
 typedef enum {
-    ITEM_INVALID,       //Invalid file/directory name or empty
-    ITEM_CHARACTER,     //Normal file/directory name, example root, home, ...
-    ITEM_STAR,          //Character *, for example /home/meo/*/
-    ITEM_DOUBLE_STAR,   //Double start **, represent directory recursive, for example /home/meo/**/file.txt
-    ITEM_MIXED_STAR     //Filename or directory name with wildcards, for example *.txt, test00*.txt ...
+    NODE_INVALID,       //Invalid file/directory name or empty
+    NODE_CHARACTER,     //Normal file/directory name, example root, home, ...
+    NODE_STAR,          //Character *, for example /home/meo/*/
+    NODE_DOUBLE_STAR,   //Double start **, represent directory recursive, for example /home/meo/**/file.txt
+    NODE_MIXED_STAR     //Filename or directory name with wildcards, for example *.txt, test00*.txt ...
 } item_t;
 
-typedef struct _splits {
+typedef struct _nodes {
     uint8_t nitems;                       //a pattern is splitted into items by '/' or '\'
+    uint8_t cur_idx;                      //current index
     uint8_t item_pos[MAX_DIR_LEVEL];      //this array store the position of each items
     item_t  item_type[MAX_DIR_LEVEL];     //type of items
     char str[MAX_PATH_LEN];               //String contains pattern
-} itemlist_t;
+} nodelist_t;
 
 typedef enum {
     MATCH,
@@ -39,19 +40,19 @@ typedef enum {
   * @param is_pattern True if this is a pattern
   * @return Return pattern struct if success, return NULL if error
   */
-itemlist_t *_translate(const char *str, uint8_t is_pattern);
+nodelist_t *_translate(const char *str, uint8_t is_pattern);
 
 /**
   * @brief Matching a filepath (splits) against a pattern
   * @return 1 if matched, 0 if not matched
   */
-int _match(itemlist_t *pattern, itemlist_t *filepath);
+int _match(nodelist_t *pattern, nodelist_t *filepath);
 
 /**
   * @brief Matching a split of filepath (at fidx position) against a split of pattern (at pidx position)
   * @return 1 if match, 0 if not match
   */
-int _match_item(itemlist_t *pattern, uint8_t pidx, itemlist_t *filepath, uint8_t fidx);
+int _match_item(nodelist_t *pattern, uint8_t pidx, nodelist_t *filepath, uint8_t fidx);
 
 /**
   * @brief Find item type of a directory/file name
@@ -64,8 +65,8 @@ item_t _find_item_type(const char *str, uint8_t start, uint8_t end);
 int fnmatch(const char *str_pattern, const char *str_filepath)
 {
     status_t status;
-    itemlist_t *pattern = _translate(str_pattern, 1);
-    itemlist_t *filepath = _translate(str_filepath, 0);
+    nodelist_t *pattern = _translate(str_pattern, 1);
+    nodelist_t *filepath = _translate(str_filepath, 0);
     if (!pattern || !filepath) {
         return -1;
     }
@@ -79,10 +80,10 @@ int fnmatch(const char *str_pattern, const char *str_filepath)
         return 1;
 }
 
-itemlist_t *_translate(const char *str, uint8_t is_pattern)
+nodelist_t *_translate(const char *str, uint8_t is_pattern)
 {
     const char *strPtr = str;
-    itemlist_t *pattern = (itemlist_t *)calloc(1, sizeof(itemlist_t));
+    nodelist_t *pattern = (nodelist_t *)calloc(1, sizeof(nodelist_t));
     char ch;
 
     if (*strPtr != '/' && *strPtr != '\\') {
@@ -136,23 +137,23 @@ item_t _find_item_type(const char *str, uint8_t start, uint8_t end)
 {
     int len = end - start;
     if (len == 1 && str[start] == '*') {
-        return ITEM_STAR;
+        return NODE_STAR;
     }
     else if (len == 2 && str[start] == '*' && str[start + 1] == '*') {
-        return ITEM_DOUBLE_STAR;
+        return NODE_DOUBLE_STAR;
     }
     else {
         int i;
         for (i = start; i < end; i++) {
             if (str[i] == '*') {
-                return ITEM_MIXED_STAR;
+                return NODE_MIXED_STAR;
             }
         }
     }
-    return ITEM_CHARACTER;
+    return NODE_CHARACTER;
 }
 
-int _match(itemlist_t *pattern, itemlist_t *filepath)
+int _match(nodelist_t *pattern, nodelist_t *filepath)
 {
     int match = 1;
     int pidx = 0;
@@ -175,19 +176,60 @@ int _match(itemlist_t *pattern, itemlist_t *filepath)
     return match;
 }
 
-int _match_item(itemlist_t *pattern, uint8_t pidx, itemlist_t *filepath, uint8_t fidx)
+int _match_item(nodelist_t *pattern, uint8_t pidx, nodelist_t *filepath, uint8_t fidx)
 {
-    item_t type = ITEM_TYPE(pattern, pidx);
-    if (type == ITEM_CHARACTER) {
-        int plen = ITEM_LEN(pattern, pidx);
-        int flen = ITEM_LEN(filepath, fidx);
+    item_t type = NODE_TYPE(pattern, pidx);
+    if (type == NODE_CHARACTER) {
+        int plen = NODE_LEN(pattern, pidx);
+        int flen = NODE_LEN(filepath, fidx);
         if (plen == flen) {
-            int cmp = strncmp(ITEM(pattern, pidx), ITEM(pattern, fidx), plen);
+            int cmp = strncmp(NODE(pattern, pidx), NODE(pattern, fidx), plen);
             if (!cmp)
                 return 1;
         }
     }
-    else if (type == ITEM_STAR) {
+    else if (type == NODE_STAR) {
+        return 1;
+    }
+    else if (type == NODE_MIXED_STAR) {
+        //TODO: For simplicity, currently just support 3  form of mixed star : abc*, *abc, a*b
+        char *start = pattern->str + pattern->item_pos[pidx];
+        char *end = pattern->str + (pattern->item_pos[pidx + 1] -1);
+        char *pptr, *fptr;
+        int idx;
+        if (*start == '*') {
+            fptr = filepath->str + (filepath->item_pos[fidx + 1] - 1);
+            for (pptr = end; pptr > start; pptr--, fptr--) {
+                if (*pptr != *fptr)
+                    return 0;
+            }
+        }
+        else if (*end == '*') {
+            fptr = filepath->str + filepath->item_pos[fidx];
+            for (pptr = start; pptr < end; pptr++, fptr++) {
+                if (*pptr != *fptr)
+                    return 0;
+            }
+        }
+        else {
+            pptr = pattern->str + pattern->item_pos[pidx];
+            fptr = filepath->str + filepath->item_pos[fidx];
+            while (*pptr != '*') {
+                if (*pptr != *fptr)
+                    return 0;
+                pptr++;
+                fptr++;
+            }
+            pptr = pattern->str + (pattern->item_pos[pidx + 1] - 1);
+            fptr = filepath->str + (filepath->item_pos[fidx + 1] - 1);
+            while (*pptr != '*') {
+                if (*pptr != *fptr)
+                    return 0;
+                pptr--;
+                fptr--;
+            }
+        }
+        return 1;
     }
     return 0;
 }
--- fnmatch/tests/test003.c
+++ fnmatch/tests/test003.c
@@ -7,34 +7,36 @@ int main(int argc, char **argv)
 {
 
     //Test 1
-    char *pattern = "/home/*/file.txt";
+    char *pattern = "/home/*eo/file.txt";
     char *filepath = "/home/meo/file.txt";
     int match = fnmatch(pattern, filepath);
     printf("%d\n", match);
 
     //Test 2
-    pattern = "/root/meo/*/file.txt";
+    pattern = "/root/meo/file.*";
     filepath = "/root/meo/file.txt";
     match = fnmatch(pattern, filepath);
     printf("%d\n", match);
 
     //Test 3
-    pattern = "/root/meo/*";
-    filepath = "/root/meo/file.txt";
+    pattern = "/root/meo/*.exe";
+    filepath = "/root/meo/file.exe";
     match = fnmatch(pattern, filepath);
     printf("%d\n", match);
 
     //Test 4
-    pattern = "/*/meo/*/a.txt";
-    filepath = "/root/meo/file/a.txt";
+    pattern = "/root/m*o/file.exe";
+    filepath = "/root/meo/file.exe";
     match = fnmatch(pattern, filepath);
     printf("%d\n", match);
 
     //Test 5
-    pattern = "/root/*/*/*";
-    filepath = "/root/";
+    pattern = "/root/meo/file.*ee";
+    filepath = "/root/meo/file.exe";
     match = fnmatch(pattern, filepath);
     printf("%d\n", match);
+
+
     return 0;
 }
 
